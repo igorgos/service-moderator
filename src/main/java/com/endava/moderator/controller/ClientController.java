ad84022d7ba000a3188ad842d367509d460d2257
igorgos <igorgos@endava.com>
2019-04-16 16:35:25 +0300

    Implementare Client

--- src/main/java/com/endava/moderator/controller/ClientController.java
+++ src/main/java/com/endava/moderator/controller/ClientController.java
@@ -4,9 +4,15 @@ import java.util.Date;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.PhaseId;
 
+import org.primefaces.behavior.ajax.AjaxBehavior;
+import org.primefaces.component.datatable.DataTable;
 import org.primefaces.event.RowEditEvent;
+import org.primefaces.util.ComponentUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +23,12 @@ import com.endava.moderator.model.Client;
 import com.endava.moderator.service.ClientService;
 
 @RestController
-@Scope("session")
+@Scope("view")
 public class ClientController {
 	private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
 	@Autowired
 	private ClientService clientService;
-	
 	private List<Client> clients;
-	private Client newClient;
 
 	public void init() {
 		if (clients == null) {
@@ -43,18 +47,10 @@ public class ClientController {
 		this.clients = clients;
 	}
 
-	public Client getNewClient() {
-		return newClient;
-	}
-
-	public void setNewClient(Client newClient) {
-		this.newClient = newClient;
-	}
-
 	public void onRowEdit(RowEditEvent event) {
 		Client selectedService = (Client) event.getObject();
 		Client savedService = clientService.save(selectedService);
-		FacesMessage msg = new FacesMessage("Client Edited", savedService.getName());
+		FacesMessage msg = new FacesMessage("Service Edited", savedService.getName());
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 
@@ -63,11 +59,32 @@ public class ClientController {
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 
-	public void addNew() {
-		Client savedClient = clientService.save(newClient);
-		clients.add(savedClient);
-		FacesMessage msg = new FacesMessage("Client Added", savedClient.getName());
-		FacesContext.getCurrentInstance().addMessage(null, msg);
+	public void onAddNew() {
+		Client newService = new Client();
+		clients.add(newService);
+		FacesMessage msg = new FacesMessage("New Service added", newService.getName());
+		FacesContext currentInstance = FacesContext.getCurrentInstance();
+		currentInstance.addMessage(null, msg);
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		try {
+
+			DataTable table = (DataTable) facesContext.getViewRoot().findComponent("form:services");
+
+			UIComponent uiTable = ComponentUtils.findParentForm(facesContext, table);
+
+			final AjaxBehavior behavior = new AjaxBehavior();
+			RowEditEvent rowEditEvent = new RowEditEvent(uiTable, behavior, table.getRowData());
+
+			rowEditEvent.setPhaseId(PhaseId.UPDATE_MODEL_VALUES);
+			table.broadcast(rowEditEvent);
+
+		} catch (AbortProcessingException e) {
+			e.printStackTrace();
+		}
+
+//        RequestContext requestContext = RequestContext.getCurrentInstance(currentInstance);
+//        requestContext.execute("$('.styleEventosPlanPrestacion tbody.ui-datatable-data tr:first-child td div.ui-row-editor a.ui-row-editor-pencil').click()");
 	}
 
 	public void doSomething() {
